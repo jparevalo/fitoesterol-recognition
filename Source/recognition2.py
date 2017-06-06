d549a2112e1eeaa42e32accf38f4cea86f00b16d
jparevalo <jparevalo@miuandes.cl>
2017-06-06 13:10:03 -0400

    Recognized ROI and painted white the trash

--- Source/recognition2.py
+++ Source/recognition2.py
@@ -6,11 +6,11 @@ max_tresh = 255
 
 
 # Detect edges using Threshold
-img = cv2.imread('../Pictures/img1.tif',0)
+img = cv2.imread('../Pictures/img3.tif',0)
 img = cv2.medianBlur(img,5)
 cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-ret,thresh1 = cv2.threshold(img,160,255,cv2.THRESH_BINARY);
+ret,thresh1 = cv2.threshold(img,170,255,cv2.THRESH_BINARY);
 # Find contours
 im2, contours, hierarchy = cv2.findContours(thresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -41,7 +41,26 @@ for i in range(len(contours)):
     #for( int j = 0; j < 4; j++ )
     #  line( drawing, rect_points[j], rect_points[(j+1)%4], color, 1, 8 );
 
+img = cv2.imread('../Pictures/img3.tif',0)
+img2 = thresh1
+
+skin_row_index = 0
+skin_pixel_index = 0
+its_skin = False
+for row in img:
+    skin_pixel_index = 0
+    for pixel in row:
+        skin_pixel = img2[skin_row_index][skin_pixel_index]
+        if skin_pixel != 255:  # WHITE
+            img2[skin_row_index][skin_pixel_index] = img[skin_row_index][skin_pixel_index]
+        else:
+            img2[skin_row_index][skin_pixel_index] = 255
+        skin_pixel_index += 1
+    skin_row_index += 1
+
+
+
 # Show in a window
-cv2.imshow('detected circles', drawing)
+cv2.imshow('detected circles', img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
