42cf66cfd65a16397efdd537cd2f26b97c32e258
jparevalo <jparevalo@miuandes.cl>
2017-06-06 15:44:24 -0400

    Applied Hough to image

--- Source/recognition2.py
+++ Source/recognition2.py
@@ -59,48 +59,23 @@ for row in img:
 
 
 # Show in a window
-cv2.imshow('detected circles', img2)
+cv2.imshow('detected circles 2', img)
 
-img = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
+img = img2
+img = cv2.medianBlur(img,5)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-b,g,r = cv2.split(img)
-rgb_img = cv2.merge([r,g,b])
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                            param1=30,param2=28,minRadius=10,maxRadius=50)
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,200,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+circles = np.uint16(np.around(circles))
+print len(circles[0,:])
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-cv2.imshow('detectedes',thresh)
-# noise removal
-kernel = np.ones((1,1),np.uint8)
-#opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
-closing = cv2.morphologyEx(thresh,cv2.MORPH_CLOSE,kernel, iterations = 10)
-
-# sure background area
-sure_bg = cv2.dilate(closing,kernel,iterations=10)
-
-# Finding sure foreground area
-dist_transform = cv2.distanceTransform(sure_bg,cv2.DIST_L2,3)
-
-# Threshold
-ret, sure_fg = cv2.threshold(dist_transform,0.1*dist_transform.max(),255,160)
-
-
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
-unknown = cv2.subtract(sure_bg,sure_fg)
-
-# Marker labelling
-ret, markers = cv2.connectedComponents(sure_fg)
-
-# Add one to all labels so that sure background is not 0, but 1
-markers = markers+1
-
-# Now, mark the region of unknown with zero
-markers[unknown==255] = 0
-
-markers = cv2.watershed(img,markers)
-img[markers == -1] = [255,0,0]
-
-cv2.imshow('detected circles',img)
+cv2.imshow('detected circles',cimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
